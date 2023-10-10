83727f4219f24531084c7e146569483061b5122d
Ankit Garg <ankitgarg3738@gmail.com>
2023-10-10 19:22:34 +0530

    // DP PROBLEMS

---
+++ longest_common_subsequence_number.cpp
@@ -0,0 +1,34 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+int solve(int arr[],int n,int curr,int prev,vector<vector<int>>& dp){
+    if(curr==n){
+        return 0;
+    }
+    int incl=0;
+    if(dp[curr][prev+1]!=-1){
+        return dp[curr][prev+1];
+    }
+    if(prev==-1 or arr[prev]<arr[curr]){
+        incl=1+solve(arr,n,curr+1,curr,dp);
+    }
+    int excl=solve(arr,n,curr+1,prev,dp);
+    int ans=max(incl,excl);
+    dp[curr][prev+1]=ans;
+    return dp[curr][prev+1];
+}
+
+
+
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    vector<vector<int>> dp(n,vector<int>(n+1,-1));
+    return solve(arr,n,0,-1,dp);
+}
+int main(){
+    int arr[]={22,33,4,5,66,7,8,99,3,45,6,77};
+    int ans=longestIncreasingSubsequence(arr,12);
+    cout<<ans<<endl;
+    return 0;
+}
