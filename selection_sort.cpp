038ae6993b7d433d9e94e22aebd00e0af3b26b79
Ankit Garg <ankitgarg3738@gmail.com>
2023-12-08 10:17:06 +0530

    // Adding more stack Problems

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -9,17 +9,22 @@ void printArray(int arr[],int n){
 }
 
 void selectionSort(int arr[],int n){
-    for (int i = 0; i < n; i++)
+    int s=0;
+    for (int i = 0; i < n-1; i++)
     {
+        int minIndex=i;
         for (int j = i+1; j < n; j++)
         {
-             if(arr[i]>arr[j]){
-                swap(arr[i],arr[j]);
+             if(arr[minIndex]>arr[j]){
+                minIndex=j;
+               
              }
         }
+        swap(arr[i],arr[minIndex]);
+         s++;
         
     }
-    
+    cout<<"No. of Swaps="<<s<<endl;
 }
 
 int main(){
--- stack_min_ele.cpp
+++ stack_min_ele.cpp
@@ -11,15 +11,37 @@ int getMin(stack<int>s){
     int ans=getMin(s);
     return min(tp, ans);
 }
+void insertAtBottom(stack<int> &st,int ele){
+    if(st.empty()){
+        st.push(ele);
+        return;
+    }
+    int elem=st.top();
+    st.pop();
+    insertAtBottom(st,ele);
+    st.push(elem);
+}
+void reverse(stack<int> &st){
+    if(st.empty()){
+        return;
+    }
+    int ele=st.top();
+    st.pop();
+    reverse(st);
+     insertAtBottom(st,ele);
+}
 int main(){
     stack<int>s;
     s.push(22);
-    s.push(2);
-    s.push(29);
-    s.push(30);
-    s.push(40);
+   
     s.push(167);
     s.push(50);
-    cout<<getMin(s)<<endl;
+    // cout<<getMin(s)<<endl;
+    cout<<s.top()<<endl;
+    reverse(s);
+    cout<<s.top()<<endl;
+    cout<<s.size()<<endl;
+    insertAtBottom(s,100);
+    cout<<s.size()<<endl;
     return 0;
 }
